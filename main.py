9b6f0952ab28da6e52753e0f31590f72ed466354
CurtisWeimar <curtisweimar2000@gmail.com>
2022-01-30 14:18:51 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+
+# The connection to discord
+client = discord.Client()
+
+
+# Decorator as an event
+# on_ready runs when bot is first set up
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+
+# Fucntion runs
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello")
+
+client.run("OTM3NDQ3MjkyNjE1NjU1NTM2.Yfb30w.9oCgK_DpRO6QE82SAaPNC2CgDT0")
+
